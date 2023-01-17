342e74ebee0d045f8290c3f209f0dd9cbafbd8d8
ArghaSaha-369 <arghasaha369@gmail.com>
2023-01-17 23:12:13 +0530

    Complete CSS added

---
+++ script.js
@@ -0,0 +1,5 @@
+let refBtn = document.querySelectorAll("option");
+let popupBtn = document.querySelector(".popup");
+let newgameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let notification = document.getElementById("notify");
